837fbc85e8c49c828d88fb1ff29af55cf3be3c63
irigyano <110440243+irigyano@users.noreply.github.com>
2025-02-25 13:54:54 +0800

    style: header color & hover effect

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,10 @@ import { SITE_TITLE } from "../consts";
 export function Header() {
   return (
     <header>
-      <a className="font-bold text-[32px] leading-10" href="/">
+      <a
+        href="/"
+        className="font-bold text-[32px] leading-10 text-primary hover:text-primary/50 duration-300"
+      >
         {SITE_TITLE}
       </a>
     </header>
